66e5cfbb315c31772064322a38705559558a6d0e
Oliver Leisi <oleisi@hotmail.com>
2023-11-02 22:23:20 +0100

    test

--- components/ads/__init__.py
+++ components/ads/__init__.py
@@ -19,16 +19,26 @@ CONF_BUTTON_IN = "Input"
 CONF_BUTTON_OUT = "Output"
 CONF_SWITCH = "Reset"
 CONF_I2C_ADDR = 0x42
+CONF_INT_POS = "Interupt positiv Edge"
+CONF_INT_NEG = "Interupt negativ Edge"
+CONF_CLOCK_WAKEUP = "Clock Wakeup"
+CONF_CLOCK_SLEEP = "Clock Sleep"
+CONF_I2C_SLEEP = "I2C Sleep"
 
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(ads),
+        cv.Optional(CONF_INT_POS, default=True): cv.boolean,
+        cv.Optional(CONF_INT_NEG, default=True): cv.boolean,
+        cv.Optional(CONF_CLOCK_WAKEUP, default=True): cv.boolean,
+        cv.Optional(CONF_CLOCK_SLEEP, default=True): cv.boolean,
+        cv.Optional(CONF_I2C_SLEEP, default=True): cv.boolean,
         cv.Optional(CONF_BUTTON_IN): binary_sensor.binary_sensor_schema(),
         cv.Optional(CONF_BUTTON_OUT): binary_sensor.binary_sensor_schema(),
         cv.Optional(CONF_SWITCH): switch.switch_schema(),
     }
-).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
+).extend(i2c.i2c_device_schema(CONF_I2C_ADDR)).extend(cv.polling_component_schema('1s'))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -41,4 +51,6 @@ async def to_code(config):
     if CONF_BUTTON_OUT in config:
         button_out = await binary_sensor.new_binary_sensor(config[CONF_BUTTON_OUT])
         cg.add(var.set_button(button_out))
+    if CONF_SWITCH in config:
+        switch.new_switch(conf, value)
 

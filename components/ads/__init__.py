bd308c88bf4fa072d0130978ae5b8df052f18aa4
Oliver Leisi <oleisi@hotmail.com>
2023-11-02 21:52:10 +0100

    Test

--- components/ads/__init__.py
+++ components/ads/__init__.py
@@ -17,8 +17,8 @@ miniencoderc_ns = cg.esphome_ns.namespace('ads')
 ads = miniencoderc_ns.class_('ads', i2c.I2CDevice, cg.Component)
 CONF_BUTTON_IN = "Input"
 CONF_BUTTON_OUT = "Output"
-CONF_SWITCH = "Reset_"
-
+CONF_SWITCH = "Reset"
+CONF_I2C_ADDR = 0x42
 
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
@@ -28,7 +28,7 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
         cv.Optional(CONF_BUTTON_OUT): binary_sensor.binary_sensor_schema(),
         cv.Optional(CONF_SWITCH): switch.switch_schema(),
     }
-).extend(i2c.i2c_device_schema(0x42))
+).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -41,6 +41,4 @@ async def to_code(config):
     if CONF_BUTTON_OUT in config:
         button_out = await binary_sensor.new_binary_sensor(config[CONF_BUTTON_OUT])
         cg.add(var.set_button(button_out))
-    if CONF_SWITCH in config:
-        switch_out = await switch.new_switch(config[CONF_SWITCH])
-        cg.add(var.set_switch(switch_out))
+

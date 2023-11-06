fa10d76fadb81591cc7c66d48d6238ce237819df
Oliver Leisi <oleisi@hotmail.com>
2023-11-06 11:58:16 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -16,7 +16,7 @@ void attiny::loop() {
 
 
 void attiny::write_binary(bool state) {
-  this->write_str(ONOFF(state));
+
 }
 
 
@@ -26,28 +26,27 @@ void attiny::write_float(float state) {
 }
 
 void attinyBOutput::dump_config() {
-  LOG_BINARY_OUTPUT(this);
+
 }
 
 void attinyBOutput::write_state(bool state) {
-  this->parent_->write_binary(state);
+
 }
 
 void attinyFOutput::dump_config() {
-  LOG_FLOAT_OUTPUT(this);
+
 }
 
 void attinyFOutput::write_state(float state) {
-  this->parent_->write_float(state);
+
 }
 
 void attinySwitch::dump_config() {
-  LOG_SWITCH("", "UART Demo Switch", this);
+
 }
 
 void attinySwitch::write_state(bool state) {
-  this->parent_->write_binary(state);
-  this->publish_state(state);
+
 }
 
 

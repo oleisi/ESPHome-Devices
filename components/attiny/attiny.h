0dbfc81e36d5cc13f794b2d8a6c5cfec848f3d1d
Oliver Leisi <oleisi@hotmail.com>
2023-11-06 14:06:13 +0100

    test

--- components/attiny/attiny.h
+++ components/attiny/attiny.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/button/button.h"
+#include "esphome/components/output/binary_output.h"
+#include "esphome/components/output/float_output.h"
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
+#include "esphome/components/text_sensor/text_sensor.h"<
+#include "esphome/components/i2c/i2c.h"
+
 
 namespace esphome {
 namespace attiny {

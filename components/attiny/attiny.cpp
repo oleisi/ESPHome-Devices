3eb9febe04142cd7e73c90f16a456fcb36f20b0f
Oliver Leisi <oleisi@hotmail.com>
2023-11-06 13:57:56 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -8,19 +8,19 @@ static const char *TAG = "attiny.component";
 
 void attiny::setup() {
 
-}
+};
 
 void attiny::loop() {
 
-}
+};
 
 void attiny::dump_config(){
     ESP_LOGCONFIG(TAG, "attiny I2C component");  
-}
+};
 
 void attinyDeepSleep::dump_config() {
   LOG_SWITCH("", "UART Demo Switch", this);
-}
+};
 
 
 }  // namespace empty_i2c_component

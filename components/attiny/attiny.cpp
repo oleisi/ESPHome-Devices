d0c6261f4e0e595b063817a34e8a36bcd1f04b97
Oliver Leisi <oleisi@hotmail.com>
2023-11-06 11:55:01 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -10,42 +10,16 @@ void attiny::setup() {
 }
 
 void attiny::loop() {
-  while (this->available()) {
-    uint8_t c;
-    this->read_byte(&c);
-    this->handle_char_(c);
-  }
+
 }
 
-void attiny::handle_char_(uint8_t c) {
-  if (c == '\r')
-    return;
-  if (c == '\n') {
-    std::string s(this->rx_message_.begin(), this->rx_message_.end());
-    if (this->the_text_ != nullptr)
-      this->the_text_->publish_state(s);
-    if (this->the_sensor_ != nullptr)
-      this->the_sensor_->publish_state(parse_number<float>(s).value_or(0));
-    if (this->the_binsensor_ != nullptr)
-      this->the_binsensor_->publish_state(s == "ON");
-    this->rx_message_.clear();
-    return;
-  }
-  this->rx_message_.push_back(c);
-}
 
-void attiny::dump_config() {
-  LOG_TEXT_SENSOR("", "The Text Sensor", this->the_text_);
-  LOG_SENSOR("", "The Sensor", this->the_sensor_);
-}
 
 void attiny::write_binary(bool state) {
   this->write_str(ONOFF(state));
 }
 
-void attiny::ping() {
-  this->write_str("PING");
-}
+
 
 void attiny::write_float(float state) {
   this->write_str(to_string(state).c_str());
@@ -76,13 +50,7 @@ void attinySwitch::write_state(bool state) {
   this->publish_state(state);
 }
 
-void attinyButton::dump_config() {
-  LOG_BUTTON("", "UART Demo Button", this);
-}
 
-void attinyButton::press_action() {
-  this->parent_->ping();
-}
 
 }  // namespace attiny
 }  // namespace esphome

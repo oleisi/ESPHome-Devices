41f8b5a061a3188672bbf1f4b6a7cb7684c1e3fb
Oliver Leisi <oleisi@hotmail.com>
2023-11-06 14:15:43 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -27,7 +27,10 @@ void attinyDeepSleep::dump_config() {
 void attinyDeepSleep::write_state(bool state) {
   this->parent_->write_binary(state);
   this->publish_state(state);
-  this->parent_->set_sensor->publish_state(state);
+  if (state == 1)
+    this->parent_->set_sensor->publish_state("ON");
+  if (state == 0)
+    this->parent_->set_sensor->publish_state("OFF");
 }
 
 }  // namespace empty_i2c_component

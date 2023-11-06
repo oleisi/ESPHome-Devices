902555c92dc9c5b23427c12c3f3059042950d435
Oliver Leisi <oleisi@hotmail.com>
2023-11-06 16:14:57 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -9,9 +9,9 @@ static const char *TAG = "attiny";
 void attiny::setup() {
     // Tests
     this->enabled_->publish_state(true);
-    float sleepTime = get_SleepTime/1000;
+    float sleepTime = (float)get_SleepTime/1000;
     if (this->voltage_ != nullptr)
-      this->voltage_->publish_state(sleepTime);
+      this->voltage_->publish_state((float)sleepTime);
     // Tests ende  
 };
 

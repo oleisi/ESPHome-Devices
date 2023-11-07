44ca9faa902856aba8d177efe87dbd9b17ffd398
Oliver Leisi <oleisi@hotmail.com>
2023-11-07 15:28:12 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -41,13 +41,13 @@ void attiny::loop() {
 }
 void attiny::update() {
 
-
+/*
   if (this->read_register(0x02, DataReciev, 2) != i2c::ERROR_OK) {
     ESP_LOGE(TAG, "Attiny read Setup Failed");
     //this->mark_failed();
   }
     ESP_LOGD(TAG, "DataReciev[5]: %d", DataReciev[0]);
-    
+    */
 }
 void attiny::dump_config(){
     ESP_LOGCONFIG(TAG, "attiny I2C component");  

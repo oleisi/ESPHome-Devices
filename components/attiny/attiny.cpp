04ad32a22289a4f5a11392777e02e40422996303
Oliver Leisi <oleisi@hotmail.com>
2023-11-07 12:32:14 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -21,16 +21,9 @@ void attiny::setup() {
   bool WakeUpClock;
   bool SleepI2C;
   bool SleepClock;
-  */ 
-     
-
-};
-
-void attiny::loop() {
-
-}
-void attiny::update() {
-    uint8_t DataSetup;
+   uint16_t SleepTime;
+  uint16_t WakeTime;
+      uint8_t DataSetup;
     if (SleepI2C) {DataSetup = DataSetup + 1;};
     DataSetup = DataSetup << 1;
     if (SleepClock) {DataSetup = DataSetup + 1;};
@@ -42,7 +35,53 @@ void attiny::update() {
     if (WakeUpRising) {DataSetup = DataSetup + 1;};
    
     ESP_LOGD(TAG, "Datasetup: %d", DataSetup);
-     
+  */ 
+  uint8_t Data[5];
+    if (SleepI2C) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (SleepClock) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (WakeUpClock) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (WakeUpFalling) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (WakeUpRising) {Data[0] = Data[0] + 1;};
+    data[1] = WakeTime & 0xff;
+    data[2] = (WakeTime >> 8) & 0xff;
+    data[3] = WakeTime & 0xff;
+    data[4] = (WakeTime >> 8) & 0xff;
+    ESP_LOGD(TAG, "Data[0]: %d", Data[0]);
+    ESP_LOGD(TAG, "Data[1]: %d", Data[1]);
+    ESP_LOGD(TAG, "Data[2]: %d", Data[2]);
+    ESP_LOGD(TAG, "Data[3]: %d", Data[3]);
+    ESP_LOGD(TAG, "Data[4]: %d", Data[4]);
+
+};
+
+void attiny::loop() {
+
+}
+void attiny::update() {
+
+      uint8_t Data[5];
+    if (SleepI2C) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (SleepClock) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (WakeUpClock) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (WakeUpFalling) {Data[0] = Data[0] + 1;};
+    Data[0] = Data[0] << 1;
+    if (WakeUpRising) {Data[0] = Data[0] + 1;};
+    data[1] = WakeTime & 0xff;
+    data[2] = (WakeTime >> 8) & 0xff;
+    data[3] = WakeTime & 0xff;
+    data[4] = (WakeTime >> 8) & 0xff;
+    ESP_LOGD(TAG, "Data[0]: %d", Data[0]);
+    ESP_LOGD(TAG, "Data[1]: %d", Data[1]);
+    ESP_LOGD(TAG, "Data[2]: %d", Data[2]);
+    ESP_LOGD(TAG, "Data[3]: %d", Data[3]);
+    ESP_LOGD(TAG, "Data[4]: %d", Data[4]); 
 }
 void attiny::dump_config(){
     ESP_LOGCONFIG(TAG, "attiny I2C component");  

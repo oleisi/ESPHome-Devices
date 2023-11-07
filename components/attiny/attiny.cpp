dc26ff715ad80d7f9ab80c3d26d6e98054a0ccd1
Oliver Leisi <oleisi@hotmail.com>
2023-11-07 12:19:49 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -33,8 +33,10 @@ void attiny::setup() {
     if (WakeUpFalling) {DataSetup = DataSetup + 1;};
     DataSetup = DataSetup << 1;
     if (WakeUpRising) {DataSetup = DataSetup + 1;};
+   
+    std::string variable(DataSetup);
     ESP_LOGD(TAG, "Datasetup: ");
-    ESP_LOGD(TAG, (char*)DataSetup);
+    ESP_LOGD(TAG, string);
 };
 
 void attiny::loop() {

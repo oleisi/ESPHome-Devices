2a755236d590ac45199562536aa70e1b9906b000
Oliver Leisi <oleisi@hotmail.com>
2023-11-08 12:59:17 +0100

    test

--- components/attiny/attiny.cpp
+++ components/attiny/attiny.cpp
@@ -27,8 +27,8 @@ void attiny::update() {
     uint32_t differenz = millis()- last_time;
     last_time = millis();
     ESP_LOGE(TAG, "Zeit: %d, Differenz: %d", millis(), differenz);
-    attinyDeepSleep obj;
-    obj.write_state(true);
+    //attinyDeepSleep obj;
+    //obj.write_state(true);
   }
 
     
